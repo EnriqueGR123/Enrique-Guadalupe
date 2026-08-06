bc053e2b569d7759c79082a06aa4b013c92d8006
EnriqueGR123 <118323832+EnriqueGR123@users.noreply.github.com>
2026-08-05 23:00:35 -0600

    barra de navegacion responsiva

--- Enrique_Guadalupe/src/components/Hero.jsx
+++ Enrique_Guadalupe/src/components/Hero.jsx
@@ -1,23 +1,11 @@
-import Contact from "./Contact";
+import React from 'react'
 
-const Hero = () => {
+function Hero() {
   return (
-    <div className="">
+    <div className=' container-xl  '>
       
-      <div className="">
-        <h1>Hola, soy Enrique Guadalupe</h1>
-
-      <h2>Desarrollador full stack Junior</h2>
-
-      <p>
-        Estudiante de  Licenciatura en Inteligencia Artificial y Ciencia de Datos, con experiencia en infraestructura tecnológica, soporte a centros de datos y administración de servidores adquirida en IBM y Foxconn. Participante del programa P-TECH by IBM, con conocimientos en Python, SQL y Linux, orientado al desarrollo de soluciones de IA y automatización
-      </p>
-
-      <a href="#proyectos" className="btn">
-        Ver proyectos
-      </a></div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
--- Enrique_Guadalupe/src/components/Navbar.jsx
+++ Enrique_Guadalupe/src/components/Navbar.jsx
@@ -1,31 +1,65 @@
-const Navbar = () => {
+import React from "react";
+
+function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">MiPortafolio</div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top w-100">
+      <div className="container fluid px-4 px-lg-5 ">
+        {/* Logo */}
+        <a className="navbar-brand fw-bold " href="/">
+          Enrique Gabriel
+        </a>
 
-      <ul>
-        <li>
-          <a href="#inicio">Inicio</a>
-        </li>
+        {/* Botón hamburguesa */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        <li>
-          <a href="#sobre-mi">Sobre mí</a>
-        </li>
+        {/* Menú */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#inicio">
+                Inicio
+              </a>
+            </li>
 
-        <li>
-          <a href="#habilidades">Habilidades</a>
-        </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#sobre-mi">
+                Sobre mí
+              </a>
+            </li>
 
-        <li>
-          <a href="#proyectos">Proyectos</a>
-        </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#habilidades">
+                Habilidades
+              </a>
+            </li>
 
-        <li>
-          <a href="#contacto">Contacto</a>
-        </li>
-      </ul>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#proyectos">
+                Proyectos
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#contacto">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
